acc119d84cd4b5ebefbd5dabd5c4aed60b3f1c0c
codephil <philip@meiers.in>
2022-03-12 12:04:33 -0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import Link from "next/link";
-import fetch from "isomorphic-unfetch";
-
-const Index = ({ logs }) => {
+const Index = () => {
   return (
     <div>
       <h1>Notes</h1>
     </div>
   );
-};
-
-Index.getInitialProps = async () => {
-  const res = await fetch("http://localhost:3000/api/crud");
-  const { data } = await res.json();
-
-  return { logs: data };
 };
 
 export default Index;
