2fd85fc41563c184b8b1ef2d7a17cdcee8cc030c
codephil <philip@meiers.in>
2022-03-12 02:44:28 -0700

    model2

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 //import "../styles/globals.css";
 
 // import App from 'next/app'
+import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import Layout from "../components/Layout";
 import "../css/style.css";
