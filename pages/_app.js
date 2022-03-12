2accfbe7a2698350c7ab0663dc8a87afc7c8aa15
codephil <philip@meiers.in>
2022-03-12 02:49:41 -0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import Layout from "../components/Layout";
-import "../css/style.css";
+//import "../css/style.css";
 
 function MyApp({ Component, pageProps }) {
   return (
