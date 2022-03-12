30c75e381910add29a232fc1d033aa3fbeb188eb
codephil <philip@meiers.in>
2022-03-12 02:07:01 -0700

    fixingIT

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../utils/dbConnect";
-import LogEntry from "../../../models/LogEntry";
+import dbConnect from "../../utils/dbConnect";
+import LogEntry from "../../models/LogEntry";
 
 dbConnect();
 
