33d4320c7e19fd3a4200f6028b312061b1764828
codephil <philip@meiers.in>
2022-03-13 19:35:45 -0600

    updated mongo

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,42 +1,18 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URL;
-
-if (!MONGO_URL) {
-  throw new Error(
-    "Please define the MONGO_URL environment variable inside .env.local"
-  );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+const connection = {};
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+  if (connection.isConnected) {
+    return;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
+  const db = await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  connection.isConnected = db.connections[0].readyState;
 }
 
 export default dbConnect;
