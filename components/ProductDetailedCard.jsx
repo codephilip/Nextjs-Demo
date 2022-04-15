35252fd5ddef55a6f7720d08e2dd3757913c7596
codephil <philip@meiers.in>
2022-04-15 17:44:20 -0600

    ProductDetailedCard update

--- components/ProductDetailedCard.jsx
+++ components/ProductDetailedCard.jsx
@@ -4,17 +4,21 @@ import Link from "next/link";
 
 const ProductDetailedCard = ({ product }) => {
   return (
-    <div class="mt-10 max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-      <div class="flex items-center px-6 py-3 bg-gray-900">
-        <h1 class="mx-3 text-lg font-semibold text-white">{product.title}</h1>
-      </div>
+    <div class="mt-10 bg-white w-1/2 mx-auto p-2 sm:p-4 sm:h-64 rounded-2xl shadow-lg flex flex-col sm:flex-row gap-5 select-none ">
+      <img
+        class="h-52 sm:h-full sm:w-72 rounded-xl bg-gray-100"
+        src={product.img}
+      />
 
-      <div class="px-6 py-4">
-        <p class="py-2 text-gray-700 dark:text-gray-400">
-          {product.secondary_desc}
-        </p>
-
-        <p>{product.CPT}</p>
+      <div class="flex flex-col flex-1 gap-5 sm:p-2">
+        <div class="flex flex-1 flex-col gap-3">
+          <div class="bg-gray-100 w-full h-14 rounded-2xl">
+            <h1 class="text-center">{product.title}</h1>
+          </div>
+          <p class="text-center">{product.desc}</p>
+          <p class="text-center">{product.secondary_desc}</p>
+          <p class="text-center"></p>
+        </div>
       </div>
     </div>
   );
