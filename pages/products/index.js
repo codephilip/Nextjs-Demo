74f037d2e17e9f733a489b257c4d9374219a4a64
codephil <philip@meiers.in>
2022-04-15 16:01:11 -0600

    remove product-form (keep develop clean)

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 import ProductList from "../../components/ProductList";
-import ProductForm from "../../components/ProductForm";
 export default function Home({ productList }) {
   return (
     <div>
       <h1 class="text-center pt-10">Radiology</h1>
-      <ProductForm />
       <ProductList productList={productList} />
     </div>
   );
