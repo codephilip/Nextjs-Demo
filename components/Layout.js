5272de3c78866c9b28dfbc38a6c68904b867e233
Solrovivrus <77756366+Solrovivrus@users.noreply.github.com>
2022-04-25 21:17:27 -0600

    changes homepage

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,6 @@ const Layout = ({ children }) => (
   <>
     <Head>
       <title>Medical Transparency</title>
-      <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
-      <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/> 
-      <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
     </Head>
     <Header />
     {children}
--- pages/index.js
+++ pages/index.js
@@ -7,53 +7,51 @@ import styles from "../styles/Home.module.css";
 
 export default function Home({ productList }) {
   return (
-    <div className={styles.HomePageContent}>
-      <div style={{ display: "flex" }}>
-        <div className={styles.hpcSideColumn}>
-          <p className={styles.hpMainMessage}>
-            WE WANT YOU TO SEE WHAT WE SEE.
-          </p>
-        </div>
-        <Image src={doctorimg1} alt="Hero" width={1005} height={698} />
-      </div>
-      <div className={styles.break}></div>
-      <div className={styles.ourStory}>
-        <h1
-          style={{
-            fontFamily: "Bebas Neue",
-            padding: "10px",
-            fontSize: "100px",
-            color: "#234473",
-            textAlign: "center",
-          }}
-        >
-          OUR STORY
-        </h1>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          pharetra sollicitudin orem ac ultricies. Vestibulum eget mollis dolor.
-          Donec finibus mi et quam condimentum efficitur. Nam in efficitur quam.
-          Sed bibendum nunc odio, vitae iaculis nisl interdum quis. Morbi
-          sollicitudin molestie nulla non lobortis. Phasellus suscipit consequat
-          condimentum. Aliquam nibh tortor, molestie eu eleifend in, feugiat
-          scelerisque erat. Nam vulputate lectus in tortor sollicitudin rhoncus.
-          Etiam et sem eu ipsum ultrices ornare quis sit amet risus.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
-      <div style={{ display: "flex" }}>
-        <div className={styles.ourStoryImage}>
-          <Image src={doctorimg2} alt="Hero" width={600} height={1000} />
-        </div>
-        <div className={styles.ourStorySideQuote}>
-          <p>"LOREM IPSUM DOLOR SIT AMET"</p>
-        </div>
-      </div>
+    <section class="text-gray-600 body-font bg-beige">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center flow-root">
+      <h1 class=" ml-32 title-font text-8xl mb-4 font-BebasNeue text-blue float-left">
+        WE 
+        <br/>WANT 
+        <br/>YOU
+        <br/>TO
+        <br/>SEE
+        <br/>WHAT
+        <br/>WE
+        <br/>SEE
+      </h1>
+      
     </div>
+    <div class="max-w-full h-auto">
+        <Image src={doctorimg1} alt="Hero" class="object-coverobject-center rounded" />
+    </div>
+  </div>
+  
+  <div class="container px-5 py-24 mx-auto">
+    <div class="max-w-full mx-auto text-center">
+    <p class="leading-relaxed text-8xl font-BebasNeue text-blue">OUR STORY</p>
+    <br/>
+      <p class="md:ml-52 md:mr-56 text-3xl font-Inter leading-relaxed font-Roboto text-left">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <br/>
+        <br/>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </div>
+  </div>
+
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <Image src={doctorimg2} alt="Hero" class="object-coverobject-center rounded" />
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font text-8xl mb-4 font-BebasNeue text-blue">"Lorem ipsum dolor sit amet"</h1>     
+    </div>
+  </div>
+</section>
   );
 }
 
