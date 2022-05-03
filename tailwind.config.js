2f7604d96717a5e853cea99d1bb6619fa36392c2
codephil <philip@meiers.in>
2022-05-02 22:10:02 -0600

    css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,20 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [
-    plugin(function ({ addBase, theme }) {
-      addBase({
-        h1: { fontSize: theme("fontSize.4xl") },
-        h2: { fontSize: theme("fontSize.xl") },
-        h3: { fontSize: theme("fontSize.lg") },
-      });
-    }),
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'redd': '#243c5a',
-      },
-    }
-  }
+  // plugins: [
+  //   plugin(function ({ addBase, theme }) {
+  //     addBase({
+  //       h1: { fontSize: theme("fontSize.4xl") },
+  //       h2: { fontSize: theme("fontSize.xl") },
+  //       h3: { fontSize: theme("fontSize.lg") },
+  //     });
+  //   }),
+  // ],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       redd: "#243c5a",
+  //     },
+  //   },
+  // },
 };
