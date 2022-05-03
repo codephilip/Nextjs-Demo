45f1fab53a79f2d7590e68c9a5937b0122d6d5e1
codephil <philip@meiers.in>
2022-05-02 22:15:33 -0600

    demo config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/*.{js,ts,jsx,tsx}", "./components/*.{js,ts,jsx,tsx}"],
   // plugins: [
   //   plugin(function ({ addBase, theme }) {
   //     addBase({
