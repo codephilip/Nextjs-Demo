9b1029a5b936913a97f4230e6e9593fd5916dcf2
Solrovivrus <77756366+Solrovivrus@users.noreply.github.com>
2022-05-03 18:43:15 -0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -88,13 +88,12 @@ const Header = () => {
                 
                 <div class="relative px-5">
                     <span class="absolute inset-y-0 left-5 flex items-center pl-3">
-                        <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
+                    <SearchIcon onClick={search} className="h-6 text-gray-500 flex-shrink cursor-pointer" />
                     </span>
 
-                    <input type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search"/>
+                    <input ref={searchInputRef} className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" type="text" placeholder="Search Services" onSubmit={search}/>
                 </div>
+                
             </div>
         
         <div>
