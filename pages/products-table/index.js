64db4fa72bcf69050d7712b3a7b29f3dd5a74afa
codephil <philip@meiers.in>
2022-05-03 21:40:29 -0600

    products table api

--- pages/products-table/index.js
+++ pages/products-table/index.js
@@ -72,7 +72,7 @@ export default function Home({ products }) {
 export const getServerSideProps = async () => {
   // const res = await axios.get("http://localhost:3000/api/products");
   const res = await axios.get(
-    "https://cranky-banach-68238c.netlify.app//api/products"
+    "https://cranky-banach-68238c.netlify.app/api/products"
   );
   console.log(res);
   return {
