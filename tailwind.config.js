7db418c6010728433db30bae92e3822a04828021
codephil <philip@meiers.in>
2022-05-03 21:44:30 -0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.4xl") },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+      });
+    }),
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#243c5a",
+      },
+    },
   },
-  plugins: [],
 };
