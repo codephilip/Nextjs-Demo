ecd2f10edd9d2d90ce274371af7a554a4bfab8ea
codephilip <philip@meiers.in>
2022-05-03 21:27:16 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,31 @@
+// const plugin = require("tailwindcss/plugin");
 
-const plugin = require("tailwindcss/plugin");
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   plugins: [
+//     plugin(function ({ addBase, theme }) {
+//       addBase({
+//         h1: { fontSize: theme("fontSize.4xl") },
+//         h2: { fontSize: theme("fontSize.xl") },
+//         h3: { fontSize: theme("fontSize.lg") },
+//       });
+//     }),
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         red: "#243c5a",
+//       },
+//     },
+//   },
+// };
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  plugins: [
-    plugin(function ({ addBase, theme }) {
-      addBase({
-        h1: { fontSize: theme("fontSize.4xl") },
-        h2: { fontSize: theme("fontSize.xl") },
-        h3: { fontSize: theme("fontSize.lg") },
-      });
-    }),
-  ],
-  theme: {
-    extend: {
-      colors: {
-        redd: "#243c5a",
-      },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
